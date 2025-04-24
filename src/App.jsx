9a9b4392992a7eff7f1ef0d39e8689616cd344f0
Miha Jan Strehovec <48392708+mihajanstrehovec@users.noreply.github.com>
2025-04-24 19:36:22 +0200

    style improvments

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div style={{ padding: '1rem' }}>
       
-      <Typography fontSize={30} fontWeight={600} mb={4}>Document scanner</Typography>
+      <Typography fontSize={36} fontWeight={600} mb={4}>Document scanner</Typography>
       <IDBoltScanner />
       <Stack alignItems={'end'} justifyContent={'flex-end'} display={'flex'} direction={'row'} spacing={1}>
         <Typography fontSize={11} sx={{color: '#111927'}}>Powered by</Typography>
